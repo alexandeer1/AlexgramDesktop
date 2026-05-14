edc5dc536f19a7554a8b9789a6021f923d41bd75
raushan-maker <raushan99goh@gmail.com>
2026-05-14 21:54:25 +0530

    Final fixes for Windows, Linux, and Mac builds: library paths, Docker naming, and Mac compilation

--- Telegram/ThirdParty/fcitx5-qt/qt6/platforminputcontext/qtkey.h
+++ Telegram/ThirdParty/fcitx5-qt/qt6/platforminputcontext/qtkey.h
@@ -1,1 +1,16 @@
-../../qt5/platforminputcontext/qtkey.h
+/*
+ * SPDX-FileCopyrightText: 2017~2017 CSSlayer <wengxt@gmail.com>
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ *
+ */
+
+#ifndef _PLATFORMINPUTCONTEXT_QTKEY_H_
+#define _PLATFORMINPUTCONTEXT_QTKEY_H_
+
+#include <QString>
+#include <cstdint>
+
+int keysymToQtKey(uint32_t keysym, const QString &text);
+
+#endif // _PLATFORMINPUTCONTEXT_QTKEY_H_
