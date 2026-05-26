11eed12a9c686cec89efc0c02e1827d32dca77d5
raushan-maker <raushan99goh@gmail.com>
2026-05-26 23:37:07 +0530

    fix(prepare): cd into patches directory before checking out commit

--- Telegram/build/prepare/prepare.py
+++ Telegram/build/prepare/prepare.py
@@ -454,6 +454,7 @@ if customRunCommand:
 
 stage('patches', """
     git clone https://github.com/desktop-app/patches.git
+    cd patches
     git checkout 667174b0ac0e062ee13379d585c675df50a5f837
 mac:
     git clone https://github.com/desktop-app/qt6_highsierra_patches.git qt6_highsierra
